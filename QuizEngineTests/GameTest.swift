6cf78444adc2b8b7d330ceed11309b1979ef5961
Aleksandr Ivashchenko <aleksandr.ivashchenko@sigma.software>
2017-11-10 17:20:32 +0200

    master > Add game engine implementation. Add test cases for it.

--- QuizEngineTests/GameTest.swift
+++ QuizEngineTests/GameTest.swift
@@ -3,14 +3,34 @@ import XCTest
 import QuizEngine
 
 class GameTest: XCTestCase {
-  
-  func test_startGame_answersOneOutOfTwoCorrectly_scores() {
-    let router = RouterSpy()
-    startGame(questions: ["Q1", "Q2"], router: router, correctAnswers: ["Q1": "A1", "Q2": "A2"])
     
-    router.answerCallback("A1")
-    router.answerCallback("wrong")
-
-    XCTAssertEqual(router.routedResult!.score, 1)
-  }
+    var router = RouterSpy()
+    var game: Game<String, String, RouterSpy>!
+    
+    override func setUp() {
+        super.setUp()
+        
+        game = startGame(questions: ["Q1", "Q2"], router: router, correctAnswers: ["Q1": "A1", "Q2": "A2"])
+    }
+    
+    func test_startGame_answersZeroOutOfTwoCorrectly_scoresZero() {
+        router.answerCallback("wrong")
+        router.answerCallback("wrong")
+        
+        XCTAssertEqual(router.routedResult!.score, 0)
+    }
+    
+    func test_startGame_answersOneOutOfTwoCorrectly_scoresOne() {
+        router.answerCallback("A1")
+        router.answerCallback("wrong")
+        
+        XCTAssertEqual(router.routedResult!.score, 1)
+    }
+    
+    func test_startGame_answersTwoOutOfTwoCorrectly_scoresTwo() {
+        router.answerCallback("A1")
+        router.answerCallback("A2")
+        
+        XCTAssertEqual(router.routedResult!.score, 2)
+    }
 }
